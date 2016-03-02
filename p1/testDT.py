5f22f18ab1b54e688adc3d57ae86b53f58d882e3
yfujimot <yosh.fuj@gmail.com>
2016-03-02 11:10:13 -0500

    all tests

--- p1/testDT.py
+++ p1/testDT.py
@@ -27,3 +27,12 @@ print(datasets.SentimentData.words[3842])
 print(datasets.SentimentData.words[3892])
 
 runClassifier.trainTestSet(dt.DT({'maxDepth': 1}), datasets.SentimentData)
+runClassifier.trainTestSet(dt.DT({'maxDepth': 3}), datasets.SentimentData)
+runClassifier.trainTestSet(dt.DT({'maxDepth': 5}), datasets.SentimentData)
+
+curve = runClassifier.learningCurveSet(dt.DT({'maxDepth': 9}), datasets.SentimentData)
+runClassifier.plotCurve('DT on Sentiment Data', curve)
+
+# W2
+curve = runClassifier.hyperparamCurveSet(dt.DT({}), 'maxDepth', [1,2,4,6,8,12,16], datasets.SentimentData)
+runClassifier.plotCurve('DT on Sentiment Data (hyperparameter)', curve)
