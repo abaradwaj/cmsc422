b697d1d3830ad35531d3851a9372601de15aa015
yfujimot <yosh.fuj@gmail.com>
2016-04-16 20:44:35 -0400

    init linearTest

---
+++ p2/linearTest.py
@@ -0,0 +1,9 @@
+import datasets,binary,dt,gd,knn,linear,mlGraphics,multiclass,runClassifier
+
+f = linear.LinearClassifier({'lossFunction': linear.SquaredLoss(), 'lambda': 0, 'numIter': 100, 'stepSize': 0.5})
+runClassifier.trainTestSet(f, datasets.TwoDAxisAligned)
+# Training accuracy 0.91, test accuracy 0.86
+f
+# w=array([ 2.73466371, -0.29563932])
+mlGraphics.plotLinearClassifier(f, datasets.TwoDAxisAligned.X, datasets.TwoDAxisAligned.Y)
+show(False)
