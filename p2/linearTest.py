144a4e69e1e70f2e7f6d44d3633cc90492f99754
yfujimot <yosh.fuj@gmail.com>
2016-04-20 17:14:15 -0400

    script to detect top positive and negative words

--- p2/linearTest.py
+++ p2/linearTest.py
@@ -50,16 +50,47 @@ from numpy import *
 
 # WU5
 print("Logistic:")
-# f = linear.LinearClassifier({'lossFunction': linear.LogisticLoss(), 'lambda': 1, 'numIter': 100, 'stepSize': 0.5})
-# runClassifier.trainTestSet(f, datasets.WineDataBinary)
-# print(f)
+f = linear.LinearClassifier({'lossFunction': linear.LogisticLoss(), 'lambda': 1, 'numIter': 100, 'stepSize': 0.5})
+runClassifier.trainTestSet(f, datasets.WineDataBinary)
 
-print("Hinge")
+large = [   0.606423261902,
+            0.689199007903,
+            0.710890552154,
+            0.770124769156,
+            0.883289753118  ]
+
+small = [   -1.1695212164,
+            -0.765309390643,
+            -0.683593167789,
+            -0.629590728143,
+            -0.532191672468 ]
+
+# f.weights.sort()
+# print(f.weights)
+
+print("Printing WineDataBinary.words:")
+print(datasets.WineDataBinary.words)
+print("Words array length: %d\n" % len(datasets.WineDataBinary.words))
+
+print("Printing small items:")
+for i,j in enumerate(f.weights):
+    for smallItem in small:
+        if ("%.8f" % j) == ("%.8f" % smallItem):
+            print datasets.WineDataBinary.words[i]
+
+print("\n")
+print("Printing large items:")
+for i,j in enumerate(f.weights):
+    for largeItem in large:
+        if ("%.8f" % j) == ("%.8f" % largeItem):
+            print datasets.WineDataBinary.words[i]
+#
+# print("Hinge")
 # f = linear.LinearClassifier({'lossFunction': linear.HingeLoss(), 'lambda': 1, 'numIter': 100, 'stepSize': 0.5})
 # runClassifier.trainTestSet(f, datasets.WineDataBinary)
 # print(f)
-
-print("Squared")
-f = linear.LinearClassifier({'lossFunction': linear.SquaredLoss(), 'lambda': 1, 'numIter': 100, 'stepSize': 0.05})
-runClassifier.trainTestSet(f, datasets.WineDataBinary)
-print(f)
+#
+# print("Squared")
+# f = linear.LinearClassifier({'lossFunction': linear.SquaredLoss(), 'lambda': 1, 'numIter': 100, 'stepSize': 0.05})
+# runClassifier.trainTestSet(f, datasets.WineDataBinary)
+# print(f)
